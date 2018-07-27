3c0871d1f12ebb074c3a11c5b57067557677ba46
Denis Molodtsov <molodtsovdenis@gmail.com>
2018-07-26 21:53:52 -0400

    Moved the images to the separate folder. Cleander readme. Minor cleaning

--- CompleteResourceMapping.ps1
+++ CompleteResourceMapping.ps1
@@ -2,7 +2,7 @@
 Connect-PnPOnline -Url $TARGET_SITE_URL
 
 $lists = Get-PnPList -Includes Views,Fields,DefaultView
-$lists = $lists | where hidden -eq $false
+$lists = $lists | Where-Object hidden -eq $false
 
 $resources = Import-Csv -Path .\resourceMapping.csv
 $resources[0].newId = $TARGET_SITE_URL
@@ -11,16 +11,16 @@ $lists | ForEach-Object{
     $line = "" | select resource,oldId,newId
     $line.resource = $_.RootFolder.ServerRelativeUrl.Replace($_.ParentWebUrl,"")
     $line.newId = $_.ID
-    $resource = $resources | where resource -eq $line.resource
+    $resource = $resources | Where-Object resource -eq $line.resource
     if ($resource -ne $null){
        $resource.newId = $line.newId
     }
 
-    $line = "" | select resource,oldId,newId
+    $line = "" | Select-Object resource,oldId,newId
     $line.resource = $_.DefaultView.ServerRelativeUrl.Replace($_.ParentWebUrl,"")
     $line.newId = $_.DefaultView.ID
-    
-    $resource = $resources | where resource -eq $line.resource
+
+    $resource = $resources | Where-Object resource -eq $line.resource
     if ($resource -ne $null){
         $resource.newId = $line.newId
     }
